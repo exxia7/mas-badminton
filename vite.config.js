dccc31ada7f633194b22d8b1e50ce9a4ff700a29
Emily Xia <exxia0201@gmail.com>
2025-01-12 02:56:44 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/mas-badminton/',
+  base: '/', // Use '/' for custom domains or '/your-repo-name/' for GitHub Pages with repo hosting
+  build: {
+    outDir: 'dist', // The default output folder
+    assetsDir: 'assets', // The folder inside dist where assets are placed
+  },
 });
